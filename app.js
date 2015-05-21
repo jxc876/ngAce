e6c65cafad36c92ae6d30b5349e5fd266fa236e5
mramirez <jxc876@gmail.com>
2015-05-21 10:03:51 -0500

    add readonly property

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 (function(){
     var app = angular.module('app',[]);
+
+
     app.controller('MainCtrl', function($scope){
     	$scope.text = 'Hello from Angular';
+        $scope.isReadOnly = true;
     });
 
     app.directive('ace', function(){
     	return {
+            scope: {
+                readOnly: '='
+            },
     		restrict: 'A',
     		link: function(scope, elem, attrs){
     			var node = elem[0];
     			var editor = ace.edit(node);
     			editor.setTheme("ace/theme/monokai");
     			editor.getSession().setMode("ace/mode/markdown");
+                editor.setReadOnly(scope.readOnly);
     		}
     	};
     });
